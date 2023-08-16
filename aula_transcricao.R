ccb5b25c815819440b6699b5da3d303f274c1c90
juciane <jhucyy.pereira@gmail.com>
2023-08-15 23:24:59 -0300

    Exercícios I

---
+++ aula_transcricao.R
@@ -0,0 +1,41 @@
+# R é uma linguagem de programação desenvolvida especificamente 
+# para análise de dados e visualização de dados. 
+
+# tudo no r é um objeto, e um objeto é uma forma de guardar a informação;
+
+#os operadores lógicos ou também, operadores boolianos. 
+#Podem ser utilizados para FALSE or TRUE.
+#Faça perguntas aos operadores lógicos
+
+# == igual a 
+# != não é igual a 
+#obs: o sinal de exclamação no r traduz para "não" ou seja,
+# != "não é igual a "
+#maior que >
+#menor que <
+# maior ou igual a  >=
+# menor ou igual a  <=
+# | "ou" 
+# & "e"
+#obs: esses dois últimos são usados para combinar
+#mais de uma operação.
+# %in% permite fazer operações lógicas entre vetores.
+
+
+# Vetores -----------------------------------------------------------------
+
+#vetores são conjunto de valores concatenados em um mesmo objeto. 
+
+
+# Variáveis -----------------------------------------------------------------
+
+#variáveis são os tipos de objetos presentes na linguagem R.
+
+#os principais tipos são: logical, numeric, character (texto), factors.
+
+# Funções -----------------------------------------------------------------
+
+#tudo que "acontece" no R é uma função,
+#cada função tem um argumento. 
+
+
